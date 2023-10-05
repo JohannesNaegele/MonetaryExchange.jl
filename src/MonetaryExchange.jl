0ef436d9eb2c4216a67745170546d8d42e95b6d4
Johannes Nägele <johannes.naegele@adacon-consulting.de>
2023-10-05 23:50:39 +0200

    Export game

--- src/MonetaryExchange.jl
+++ src/MonetaryExchange.jl
@@ -1,8 +1,11 @@
 module MonetaryExchange
 
+export Game, DefaultGame, Agent
+export bounded_cobb_douglas
+
 include("Utility.jl")
 include("Agent.jl")
-include("Group.jl")
+include("Game.jl")
 include("Process.jl")
 
 end # module MonetaryExchange
