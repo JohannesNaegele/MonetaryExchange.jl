4492704795f8f1c8e8a9ffbeac5ccfd2a1770aa7
Johannes Nägele <johannes-naegele@web.de>
2024-09-09 21:32:01 +0200

    Rethink matching

--- src/Matching.jl
+++ src/Matching.jl
@@ -1,24 +1,17 @@
 """
-We loop through all agents; one agent draws a matching with probability p = μ/2, we sample the partner uniformly from the rest.
-Thus, each agent has a probability p + (n - 1)/(n - 1)p = μ of matching with one of the other agents.
-The important difference to continuous however is that in every time step it is possible for an agent to have multiple trades.
+We loop through all agents; an agent draws a matching with Poisson(h * μ / 2), we sample the partner uniformly from the rest.
 """
 function sequential_matching(game::Game)
-    pairs::Array{Pair} = [] # ?
-    return pairs
 end
 
 """
-Build (number_of_agents * μ) pairs. If this product is not an even number:
-Choose with the appropriate Bernoulli probability p between the upper and the lower number of pairs.
-This might result in faster convergence but it is not clear that we have a economically sound interpretation.
+Choose n agents where n ∼ Poisson(h * μ * total number of agents). If n is not an even number, we take n - 1. Then we randomly assign partners with uniform probability.
 """
 function simultaneous_matching(game::Game)
-    for group in game.group
-        for agent in group.agents
-            nothing
-        end
-    end
-    pairs::Array{Pair} = [] # ?
+    poi = Poisson(game.h * game.μ * n_agents(game))
+    n_old = rand(poi)
+    n = n_old % 2 == 0 ? n_old : n_old - 1
+    matched_agents = sample(1:n_agents(game), n, replace=false)
+    pairs = matched_agents[start:div(n, 2)] .=> matched_agents[(div(n, 2) + 1):end]
     return pairs
 end
