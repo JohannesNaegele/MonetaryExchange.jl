60124233766d3d70bffcea3fd888a5eff3089735
Johannes Nägele <johannes-naegele@web.de>
2024-09-09 14:05:29 +0200

    Added generic utility function

--- src/Utility.jl
+++ src/Utility.jl
@@ -1,18 +1,35 @@
 """
+This is the generic example for a bounded and strictly concave utility function.
+"""
+function generic(consumption)
+    return 1 - exp(-prod(consumption))
+end
+
+"""
+This is the generic example after a logarithmic transformation.
+"""
+function log_generic(consumption)
+    return -prod(consumption)
+end
+
+"""
 This is a Cobb-Douglas type utility function.
 """
 function cobb_douglas(consumption, α)
-    return (consumption .^ α)
+    return prod(consumption.^α)
 end
 
 """
 Logistic transformation.
 """
-logistic_transformation(x) = 1 / (1 + exp(x))
+logistic_transformation(x) = 1 / (1 + exp(-x))
 
 """
-This is a Cobb-Douglas type utility function after a logistic-type transformation
+This is a Cobb-Douglas type utility function after a logistic-type transformation.
+
+The derivative of this function is:
+    f'(x) = exp(-x) / (1 + exp(-x))^2 * exp(-x)'
 """
 function bounded_cobb_douglas(consumption, α)
-    return logistic_transformation(cobb_douglas(consumption, α))
+    return (logistic_transformation ∘ cobb_douglas)(consumption, α)
 end
